f364608d8a84292502eda458448e3aec64a8bfac
SidK123 <siddarthkunisetty@gmail.com>
2023-06-09 11:28:01 -0400

    added comments

--- Axon_Delay.sv
+++ Axon_Delay.sv
@@ -1,3 +1,7 @@
+//This module is used to generate the biological delay that occurs between the action potential and the
+//this action potential propagating through the axon and hitting the axon terminals. We do this by setting
+//a counter and then setting the output spike equal to the input spike when we hit the delay value.
+
 `default_nettype none
 
 module Axon_Delay{
@@ -8,13 +12,13 @@ module Axon_Delay{
   logic start;
   logic [5:0] count;
   always @(posedge clock){
-    if(spike_in == 1'b1){
+    if(spike_in == 1'b1){  //When a spike is generated, we start the counter.
       start <= 1'b1;
     }
-    if(start == 1'b1){
+    if(start == 1'b1){ //If we have started the counter, then we keep adding to the counter until we've hit the delay value.
       count <= count + 1;
     }
-    if(count == delay){
+    if(count == delay){ //If we've hit the delay value, we restart our counter, and we set our output spike equal to the 1, as the delay has passed.
     begin
       start <= 1'b0;
       spike_out <= 1'b1;
@@ -22,7 +26,7 @@ module Axon_Delay{
     end
     }
     else{
-      spike_out <= 1'b0;
+      spike_out <= 1'b0; //If the delay hasn't passed, then our output spike has not spiked yet, so it is equal to 0.
     }
   }
 endmodule Axon_Delay
--- Synapse.sv
+++ Synapse.sv
@@ -10,13 +10,13 @@ module Synapse{
 
   logic [17:0] v;
   always @(posedge clock)
-    if(reset){
+    if(reset){ //If we reset, then our current goes back to 0. 
       v <= 18'b0;
     }
     else{
-      v <= vnew;
+      v <= vnew; //Otherwise, the current at the synapse is equal to the new current at our synapse.
     }
-  
-  vnew = v + ((-1 * v) >>> 4) + (s1 ? w1 : 17'b0) + (s2 ? w2 : 17'b0) + (s3 ? w3 : 17'b0);
-  assign out = v;
+  //The current at the synapse exponentially decreases, but the neurons that input to this synapse also contribute current based on if they spike or not. If they spike, we add some amount of current to our synapse, which are the different weights.
+  vnew = v + ((-1 * v) >>> 4) + (s1 ? w1 : 17'b0) + (s2 ? w2 : 17'b0) + (s3 ? w3 : 17'b0); 
+  assign out = v; //Our output current is equaL to v, which is the variable we use for current.
 endmodule Synapse
